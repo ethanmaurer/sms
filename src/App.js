8a470f0e33870909c0590be35ebe213c3d045774
Ethan Maurer <94627969+ethanmaurer@users.noreply.github.com>
2022-10-05 12:25:57 -0500

    message

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import 
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <script>
-
-        </script>
+        <script></script>
       </header>
     </div>
   );
